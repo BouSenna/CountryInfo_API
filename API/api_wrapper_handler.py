ba1852f498481679252604401a38b27be44c714f
eddsanity <eyadhossamm@gmail.com>
2020-03-16 01:42:52 +0200

    fixed test

--- API/api_wrapper_handler.py
+++ API/api_wrapper_handler.py
@@ -1,24 +1,25 @@
 from flask_restful import Resource, reqparse
-from API.api_handler import countryHandler
+from api_handler import countryHandler
 
 class wrapperHandler(Resource):
     def __init__(self):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('info')
         self.country = countryHandler()
+        self.invalid_info = {'error': 'invalid info'}, 400
+        super().__init__()
 
         
     def get(self, name):
         country_info = self.country.req_info(name)
-        
-        parser = reqparse.RequestParser()
-        parser.add_argument('info')
-        arg = parser.parse_args()
+        arg = self.parser.parse_args()
 
+        # If no specific arguments were specified, dump everything.
         if arg['info'] == None:
             return country_info, 200
+        # Else, if specified info was found, return it with success code, if not then return invalid with 400 code.
         else:
             for i in country_info:
                 if arg['info'] == i:
                     return country_info[i], 200
-            return None, 400
+            return self.invalid_info
--- API_Testing/test_api_wrapper_handler.py
+++ API_Testing/test_api_wrapper_handler.py
@@ -1,5 +1,5 @@
 import unittest
-from API.api_wrapper_handler import wrapperHandler
+from api_wrapper_handler import wrapperHandler
 
 # in-out dummy interfaces for the Parser and CountryHandler
 class dummyParser:
@@ -14,17 +14,21 @@ class dummyCountryHandler:
     def req_info(self, name):
         return self.o
     
+#class testWrapperHandler(unittest.TestCase):
+
 def test_invalid_info():
     wrapper_handler = wrapperHandler()
 
     dummy_parser = dummyParser({'info': 'bottomLevelDomain'})
     dummy_country_handler = dummyCountryHandler({"name": "Egypt", "topLevelDomain": ".eg", "capital": "Cairo"})
 
-    wrapper_handler.country = dummy_country_handler
     wrapper_handler.parser = dummy_parser
+    wrapper_handler.country = dummy_country_handler
+    
 
     resp, status_code = wrapper_handler.get('Egypt')
     assert status_code == 400
 
 
-test_invalid_info()
+if __name__ == "__main__":
+    test_invalid_info()
