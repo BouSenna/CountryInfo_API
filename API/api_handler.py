4547c3bed3e46f48164978ddd4b1854c2e85abe4
eddsanity <eyadhossamm@gmail.com>
2020-03-15 21:21:03 +0200

    fixed api handler test

--- API/api_handler.py
+++ API/api_handler.py
@@ -4,7 +4,7 @@ import requests
 class countryHandler(Resource):
 
     def req_info(self, country_name):
-        path = 'https://restcountries.eu/rest/v2/name/{}'.format(self.country_name)
+        path = 'https://restcountries.eu/rest/v2/name/{}'.format(country_name)
         response = requests.get(path)
         return response.json()[0]
 
