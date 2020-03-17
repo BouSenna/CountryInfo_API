807ac42f98562dcec50f994a7780f6a7edb40884
BouSenna <49456936+BouSenna@users.noreply.github.com>
2020-03-17 04:29:46 +0200

    Added dummy api

---
+++ Dummy_API/dummy_api_wrapper_handler.py
@@ -0,0 +1,25 @@
+from flask_restful import Resource, reqparse
+from Dummy_API.dummy_api_handler import dummyCountryHandler
+
+class dummyWrapperHandler(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('info')
+        self.country = dummyCountryHandler()
+        self.invalid_info = {'error': 'invalid info'}, 400
+        super().__init__()
+
+        
+    def get(self, name):
+        country_info = self.country.req_info(name)
+        arg = self.parser.parse_args()
+
+        # If no specific arguments were specified, dump everything.
+        if arg['info'] == None:
+            return country_info, 200
+        # Else, if specified info was found, return it with success code, if not then return invalid with 400 code.
+        else:
+            for i in country_info:
+                if arg['info'] == i:
+                    return country_info[i], 200
+            return self.invalid_info
