be39337f89972c9a81c79b8560fb358005ac34fb
BouSenna <49456936+BouSenna@users.noreply.github.com>
2020-04-18 02:04:37 +0200

    added caching logic
    This method checks if the user's request could be handled without communicating with the external API.
    If the request was handled before, it returns the cached response. If not, it sends the request to the countries API, returns the response and saves it.

--- API/api_handler.py
+++ API/api_handler.py
@@ -1,10 +1,14 @@
 from flask_restful import Resource
+import requests_cache
 import requests
 
+
 class countryHandler(Resource):
+    requests_cache.install_cache('cache')
 
     def req_info(self, country_name):
         path = 'https://restcountries.eu/rest/v2/name/{}'.format(country_name)
-        response = requests.get(path)
-        return response.json()[0]
+        self.response = requests.get(path)
+
+        return self.response.json()[0]
 
